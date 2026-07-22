03f9b7b29d149395630da8a7d2c53d296fc3b2ac
Cr1st14n54nt14g0 <cristher369@gmail.com>
2026-07-22 09:10:32 -0600

    Prueba para la preparacion de REnder

--- core/settings.py
+++ core/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic'
     'pasteleria_app',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
