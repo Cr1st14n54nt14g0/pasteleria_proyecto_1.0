97eee4110d71fb1d9173a406716265bcb73268e3
Cr1st14n54nt14g0 <cristher369@gmail.com>
2026-07-24 12:41:59 -0600

    creacion de admin

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+echo "from pasteleria_app.models import Usuarios; Usuarios.objects.create_superuser('admin', '4dm1n1234#') if not Usuarios.objects.filter(usuario='admin').exists() else None" | python manage.py shell
