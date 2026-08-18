9355f7e3fbc490aa512cb5b451c5d90ba4fb476e
Cr1st14n54nt14g0 <cristher369@gmail.com>
2026-08-18 15:17:42 -0600

    Agregar migraciones para localizacion y producto_mostrador parte 2

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --noinput
+python manage.py makemigrations pasteleria_app --noinput
+
 
 # Crear superusuario automáticamente si no existe
 echo "from pasteleria_app.models import Usuarios; Usuarios.objects.create_superuser('admin', '4dm1n1234#') if not Usuarios.objects.filter(usuario='admin').exists() else None" | python manage.py shell
