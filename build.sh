ea01ba23a11511d064816e9a43ce49e29e32fe5c
Cr1st14n54nt14g0 <cristher369@gmail.com>
2026-08-18 16:04:17 -0600

    Agregar migraciones para localizacion y producto_mostrador parte 5

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations pasteleria_app --noinput
-python manage.py migrate --noinput
+#python manage.py makemigrations pasteleria_app --noinput
+#python manage.py migrate --noinput
 
+
+python manage.py shell -c "from django.db import connection; cursor = connection.cursor(); cursor.execute(\"DELETE FROM django_migrations WHERE app='pasteleria_app'\"); print('Migraciones eliminadas')"
+python manage.py migrate pasteleria_app
+python manage.py migrate
 
 
 # Crear superusuario automáticamente si no existe
